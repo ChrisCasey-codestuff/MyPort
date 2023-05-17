16b7870d06cfd0be2bc629b3ca15120079d1bf4e
Christopher Casey <chris.casey1234@gmail.com>
2023-05-17 16:27:39 -0700

    adjusts project positioning on medium devices

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ function Header() {
       <Link href="/" className="text-gray-300 hover:text-gray-400 transition-all tracking-wider font-extralight lg:w-2/3 sm:w-1/4">Christopher Michael Casey - Software Developer</Link>
      </h1>
      <nav className="ml-10 mb-10 sm:bg-black md:bg-transparent lg:bg-transparent
-     md:bg-none lg:mt-10 lg:bg-none fixed top-0 lg:right-0 md:right-0 sm:mr-80 md:mr-4
+     lg:mt-10 fixed top-0 lg:right-0 md:right-0 sm:mr-80 md:mr-4
      lg:mr-10 lg:z-50 md:z-50 sm:z-50 md:mt-10 sm:mt-0">
       <ul className="flex lg:flex-col lg:justify-start md:flex-col md:justify-start sm:flex-row sm:justify-between sm:items-center">
       <li data-aos="fade-left" data-aos-duration="400" className="mr-4 mt-2 mb-2 ml-2">
@@ -37,13 +37,13 @@ function Header() {
   </header>
   <div className="flex flex-row justify-center mr-2">
     <a href="https://github.com/ChrisCasey-codestuff">
-      <p data-aos="fade-right" data-aos-duration="1500" className="text-gray-400 border-2 border-black hover:text-gray-500 text-2xl mr-5 p-2 mt-12 font-extralight rounded-sm">GitHub</p>
+      <p data-aos="fade-right" data-aos-duration="1500" className="text-gray-400 hover:text-gray-500 text-2xl mr-5 p-2 mt-12 font-extralight rounded-sm">GitHub</p>
     </a>
     <a href="https://www.linkedin.com/in/christopher-casey-se">
-      <p data-aos="fade-down" data-aos-duration="1500" className="text-gray-400 border-2 border-black hover:text-gray-500 text-2xl mr-5 p-2 mt-12 font-extralight rounded-sm">LinkedIn</p>
+      <p data-aos="fade-down" data-aos-duration="1500" className="text-gray-400 hover:text-gray-500 text-2xl mr-5 p-2 mt-12 font-extralight rounded-sm">LinkedIn</p>
     </a>
     <a href="/#contact">
-      <p data-aos="fade-left" data-aos-duration="1500" className="text-gray-400 border-2 border-black hover:text-gray-500 text-2xl mr-5 p-2 mt-12 font-extralight rounded-sm">Contact</p>
+      <p data-aos="fade-left" data-aos-duration="1500" className="text-gray-400 hover:text-gray-500 text-2xl mr-5 p-2 mt-12 font-extralight rounded-sm">Contact</p>
     </a>
   </div>
   </section>
