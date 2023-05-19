15238254ddeae619b0bce9f34d473f42ee083f37
Christopher Casey <chris.casey1234@gmail.com>
2023-05-18 17:29:03 -0700

    finalizes css for all three device sizes for index page

--- pages/contact.js
+++ pages/contact.js
@@ -8,8 +8,28 @@ export default function Contact() {
     <main className="flex flex-col items-center justify-center mt-10">
       <p className="text-4xl font-extralight tracking-widest text-slate-200 ml-5">Contact Me</p>
       <div className="flex flex-row mt-14 mb-32">
-        <FiMail className="mr-10 text-gray-300 mt-5" size={80}/>
-        <h3 className="font-extralight text-gray-300 leading-loose text-4xl m-5 hover:underline">Chris.Casey1234@gmail.com</h3>
+        <div>
+          <form className="flex flex-col p-5 rounded-md" action="https://getform.io/f/ab2831ce-9681-4e89-9a86-6652d337ba8c" method="POST">
+            <div className="flex flex-row pb-5 pt-5">
+              <div className="flex flex-col">
+              <p>First Name</p>
+              <input type="text" name="first name" className="m-2 rounded-md"></input>
+              </div>
+              <div className="flex flex-col">
+                <p>Last Name</p>
+              <input type="text" name="last name" className="m-2 rounded-md"></input>
+              </div>
+            </div>
+            <div className="flex flex-col">
+            <p>Email</p>
+            <input type="text" name="email" className="m-2 mb-10 rounded-md"></input>
+            </div>
+            <div className="flex flex-col">
+              <p>Message</p>
+              <textarea name="message"className="m-2 pt-10 rounded-md"></textarea>
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   )
--- pages/resume.js
+++ pages/resume.js
@@ -16,15 +16,15 @@ export default function Resume() {
       <div className="flex flex-row justify-center">
         <h2 className="text-4xl m-10 text-gray-300 tracking-widest font-extralight mr-14">My Resume</h2>
       </div>
-      <h2 className="flex text-gray-400 font-extrathin text-lg italic ml-16 mt-10 w-5/6">“True education is a kind of never ending story — a matter of continual beginnings, of habitual fresh starts, of persistent newness.”
+      <h2 className="flex text-gray-400 font-extrathin text-lg italic ml-16 mt-10 w-5/6 sm:mr-10 lg:mr-0 md:mr-0">“True education is a kind of never ending story — a matter of continual beginnings, of habitual fresh starts, of persistent newness.”
          ― J. R. R. Tolkien</h2>
-         <div className="flex flex-row justify-center w-1/2 mt-10 pl-32">
+         <div className="flex flex-row justify-center w-1/2 mt-10 lg:pl-32 md:pl-32 sm:pl-60 lg:pr-24">
          <h3 className="text-gray-300 text-4xl font-extralight">Skills</h3>
          </div>
       <div className="flex lg:flex-row md:flex-row sm:flex-col justify-center">
-      <div className="flex flex-row justify-start bg-black w-1/2 rounded-md p-5 ">
+      <div className="flex flex-row lg:justify-start md:justify-start sm:justify-center bg-black w-1/2 rounded-md p-5 sm:mr-32">
 
-        <div className="flex flex-row items-center pl-60">
+        <div className="flex flex-row items-center lg:pl-60 sm:pl-60 md:mr-0">
         <div className="flex flex-col justify-start">
           <div data-aos="fade-zoom-in" data-aos-duration="700" className="box-border m-5 p-4  border-2 border-black hover:border-gray-300 rounded-md">
             <GrReactjs size={60} alt="React"/>
@@ -57,13 +57,15 @@ export default function Resume() {
         </div>
         </div>
       </div>
-      <a className="ml-48 pl-10 mr-10" href="../public/myRes.pdf" download>
-      <div data-aos="flip-left" data-aos-duration="1000" className="flex flex-row items-end justify-end border-2 border-gray-400 hover:border-gray-300 bg-black m-5 mr-40 rounded-md p-5 w-1/2">
+
+      <a className="lg:ml-10 md:ml-14 lg:pl-10 md:pl-10 lg:mr-36 md:mr-36" href="../public/myRes.pdf" download>
+      <div data-aos="flip-left" data-aos-duration="1000" className="flex flex-row items-end justify-end border-2 border-gray-400 hover:border-gray-300 bg-black m-5 mr-40 rounded-md p-5 w-1/2 sm:ml-32 lg:ml-0 md:ml-0">
         <div className=" text-gray-300 text-2xl font-extralight p-20">
             <p className="text-gray-300 hover:text-gray-400 pl-10 pr-10 pt-5 pb-5">Download My Resume</p>
         </div>
       </div>
       </a>
+
       </div>
     </main>
   )
